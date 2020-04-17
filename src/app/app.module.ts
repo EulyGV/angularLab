b286bbbbbb8988fa7decf710beed91587de8f452
EulyGV <euly.garces.2016@upb.edu.co>
2020-04-17 12:44:38 -0500

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ShippingComponent } from './shipping/shipping.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   
